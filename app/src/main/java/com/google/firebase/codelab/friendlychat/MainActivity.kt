7f60e65c587cd037101c6cde69fb1701e3146654
kyle kwangkyu choi <kyle.kwangkyu.choi@gmail.com>
2023-02-23 16:51:25 +0900

    Added codes that take care of login process. Added firebase json file locally (omitted on git). Need to run with Firebase CLI emulator

--- app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
+++ app/src/main/java/com/google/firebase/codelab/friendlychat/MainActivity.kt
@@ -15,6 +15,7 @@
  */
 package com.google.firebase.codelab.friendlychat
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.view.Menu
@@ -23,13 +24,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.AuthUI.IdpConfig.*
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.codelab.friendlychat.BuildConfig
 import com.google.firebase.codelab.friendlychat.databinding.ActivityMainBinding
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var manager: LinearLayoutManager
+    // Firebase instance variables
+    private lateinit var auth: FirebaseAuth
 
     private val openDocument = registerForActivityResult(MyOpenDocumentContract()) { uri ->
         uri?.let { onImageSelected(it) }
@@ -39,6 +47,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // When running in debug mode, connect to the Firebase Emulator Suite.
+// "10.0.2.2" is a special IP address which allows the Android Emulator
+// to connect to "localhost" on the host computer. The port values (9xxx)
+// must match the values defined in the firebase.json file.
+        if (BuildConfig.DEBUG) {
+            Firebase.database.useEmulator("10.0.2.2", 9000)
+            Firebase.auth.useEmulator("10.0.2.2", 9099)
+            Firebase.storage.useEmulator("10.0.2.2", 9199)
+        }
 
         // This codelab uses View Binding
         // See: https://developer.android.com/topic/libraries/view-binding
@@ -62,12 +80,37 @@ class MainActivity : AppCompatActivity() {
         binding.addMessageImageView.setOnClickListener {
             openDocument.launch(arrayOf("image/*"))
         }
+        // Initialize Firebase Auth and check if the user is signed in
+        auth = Firebase.auth
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
     }
 
     public override fun onStart() {
         super.onStart()
         // Check if user is signed in.
-        // TODO: implement
+        if (auth.currentUser == null) {
+            // Not signed in, launch the Sign In activity
+            startActivity(Intent(this, SignInActivity::class.java))
+            finish()
+            return
+        }
+    }
+
+    private fun getPhotoUrl(): String? {
+        val user = auth.currentUser
+        return user?.photoUrl?.toString()
+    }
+
+    private fun getUserName(): String? {
+        val user = auth.currentUser
+        return if (user != null) {
+            user.displayName
+        } else ANONYMOUS
     }
 
     public override fun onPause() {
@@ -104,7 +147,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun signOut() {
-        // TODO: implement
+        AuthUI.getInstance().signOut(this)
+        startActivity(Intent(this, SignInActivity::class.java))
+        finish()
     }
 
     companion object {
